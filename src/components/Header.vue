627eac6b4195a2e5657c182918e210d3155aaada
balaji <balajidontulofficial@outlook.com>
2021-04-27 20:04:33 +0530

    starting implementing component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,20 @@
 <template>
     <header>Task Tracker</header>
 </template>
+
+
+<script>
+export default {
+    name :'Header',
+}
+</script>
+
+<style scoped>
+header{
+    display: flex;
+    
+}
+</style>>
+
+
+</style>
