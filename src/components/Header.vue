424f17e887f5f881012b3c8ce4fe317318e69653
balaji <balajidontulofficial@outlook.com>
2021-04-28 18:29:30 +0530

    Deleted

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,31 +1,9 @@
 <template>
-    <header>{{title}}</header>
     
-
 </template>
 
-
 <script>
-
-
-
 export default {
-    name :'Header',
-
-    
-    props:{
-        title :String,
-},
-    
+    name:Header,
 }
 </script>
-
-<style scoped>
-header{
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 20px;
-}
-</style>>
-
